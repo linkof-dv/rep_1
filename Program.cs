650a6295265196b0495ada24879baea423880623
linkof-dv <linkof-dv@outlook.com>
2022-12-13 16:24:31 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+
+namespace Program1
+{
+    class Programm
+    {
+
+        static void Main()
+
+        { 
+           string data;
+            Console.WriteLine("Ввести чмсло");
+
+           data= Console.ReadLine();
+
+            //string n = "a";
+
+            //NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            //{
+            //NumberDecimalSeparator=".",
+            //};    
+                //double a = double.Parse(data, numberFormatInfo);
+           
+            
+                //data = "1";
+            try
+            {
+                int a = int.Parse(data);
+                // a= Convert.ToDouble(n);  
+
+                Console.WriteLine('1');
+            }
+            catch(Exception)
+            
+            {
+                Console.WriteLine("ошибка");
+            }
+
+        }
+
+    }
+
+
+}
+
